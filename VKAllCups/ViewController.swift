7b64d1e86fb43d7219e7ff64268907dc0f2a93dc
AlexeyOkhin <k-i-ll-e-r@list.ru>
2022-12-03 13:03:01 +0500

    Initial Commit

---
+++ VKAllCups/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKAllCups
+//
+//  Created by Djinsolobzik on 03.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
