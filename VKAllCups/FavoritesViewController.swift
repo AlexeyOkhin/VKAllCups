1c3ce132bbb59a5e8b6763aef952d77c727d6ec3
AlexeyOkhin <k-i-ll-e-r@list.ru>
2022-12-03 14:46:03 +0500

    settings project

--- VKAllCups/FavoritesViewController.swift
+++ VKAllCups/FavoritesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesViewController.swift
 //  VKAllCups
 //
 //  Created by Djinsolobzik on 03.12.2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
     }
 
-
 }
-
